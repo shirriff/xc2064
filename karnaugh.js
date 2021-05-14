58f084792e1c6b3cb9449123f1775baf8c07f71c
Ken Shirriff <ken.shirriff@gmail.com>
2021-05-13 23:17:31 -0700

    Change karnaugh to 0,1,2,3 output variables

--- karnaugh.js
+++ karnaugh.js
@@ -5,21 +5,21 @@
  * Variable names v0, v1, v2 are substituted into formula.
  */
 function formula3(n, v0, v1, v2) {
-  // Watch out for replacing the replacement. e.g. replace('A', 'B').replace('B', 'C') replaces A with C.
+  // Watch out for replacing the replacement. e.g. replace('V0', 'V1').replace('V1', 'V2') replaces V0 with V2.
   // That's why the formulas use lower case.
-  return formulaString((n << 8) | n).replace('a', v0).replace('b', v1).replace('c', v2); // n << 8 | n makes the formula not depend on D, i.e. 3-term formula
+  return formulaString((n << 8) | n).replaceAll('0', v0).replaceAll('1', v1).replaceAll('2', v2); // n << 8 | n makes the formula not depend on V3, i.e. 3-term formula
 }
 
 function formula4(n, v0, v1, v2, v3) {
-  return formulaString(n).replace('a', v0).replace('b', v1).replace('c', v2).replace('d', v3);
+  return formulaString(n).replaceAll('0', v0).replaceAll('1', v1).replaceAll('2', v2).replaceAll('3', v3);
 }
 
 // Generate a formula given a truth table of four variables.
 // Uses a greedy Karnaugh map algorithm. I.e. grab single-variable terms that work, then two-variable terms, three-variable terms, finally four-variable terms.
 // Data structure to hold truth table: 16-bit number where bit 1<<n indicates the output value (0, 1) for input DCBA (converted to a binary index)
 // E.g. 0000000000000001 indicates the only 1 output is for f(0, 0, 0, 0)
-// 1010101010101010 indicates 1 output if A is set, i.e. f(A, B, C, D) = A.
-// Output is a string e.g. "a + ~b * c"
+// 1010101010101010 indicates 1 output if V0 is set, i.e. f(V0, V1, V2, V3) = V0.
+// Output is a string e.g. "0 + ~1 * 2", where 0 indicates variable 0, 1 indicates variable 1, 2 indicates variable 2
 
 function formulaString(table) {
   // Handle special cases (constant output)
@@ -51,9 +51,9 @@ function formulaString(table) {
   }
 
   // Recursive function to try terms
-  // Inputs: query: [A, B, C, D] values 1, 0, or -1 (don't care)
+  // Inputs: query: [V0, V1, V2, V3] values 1, 0, or -1 (don't care)
   //   terms: number of terms to add
-  //   index: the position in query to start updating. E.e. index == 2 indicates that A and B are set but C and D can be modified
+  //   index: the position in query to start updating. E.e. index == 2 indicates that V0 and V1 are set but V2 and V3 can be modified
   function testQueries(query, terms, index) { 
     if (done) {
       return;
@@ -83,13 +83,13 @@ function formulaString(table) {
 }
 
 // Generate the truth table for a product term specified by "query".
-// A query is a vector [A, B, C, D] where 1 indicates the variable must be 1, 0 indicates the variable must be 0, and -1 indicates don't care.
+// V0 query is a vector [V0, V1, V2, V3] where 1 indicates the variable must be 1, 0 indicates the variable must be 0, and -1 indicates don't care.
 function generatePotentialTable(query) {
-  const A = 0xaaaa; // Truth table for f(A, B, C, D) = A
-  const B = 0xcccc; // Truth table for f(A, B, C, D) = B
-  const C = 0xf0f0; // Truth table for f(A, B, C, D) = C
-  const D = 0xff00; // Truth table for f(A, B, C, D) = D
-  const terms = [A, B, C, D];
+  const V0 = 0xaaaa; // Truth table for f(V0, V1, V2, V3) = V0
+  const V1 = 0xcccc; // Truth table for f(V0, V1, V2, V3) = V1
+  const V2 = 0xf0f0; // Truth table for f(V0, V1, V2, V3) = V2
+  const V3 = 0xff00; // Truth table for f(V0, V1, V2, V3) = V3
+  const terms = [V0, V1, V2, V3];
 
   let table = 0xffff;
   for (let i = 0; i < 4; i++) {
@@ -103,14 +103,14 @@ function generatePotentialTable(query) {
 }
 
 // Converts a query into a term
-// E.g. [0, 1, -1, -1] -> "~A * B"
+// E.g. [0, 1, -1, -1] -> "~V0 * V1"
 function queryToTerm(query) {
   let parts = [];
   for (let i = 0; i < 4; i++) {
     if (query[i] == 0) {
-      parts.push("~" + "abcd"[i]);
+      parts.push("~" + "0123"[i]);
     } else if (query[i] == 1) {
-      parts.push("abcd"[i]);
+      parts.push("0123"[i]);
     }
   }
   return parts.join(" * ");
